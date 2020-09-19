bc09f761160c80f3af77f5de7df5896496bd5275
ardhichoiruddin <ardhichoiruddin@yandex.com>
2020-09-19 12:46:26 +0700

    first update

---
+++ components/Box.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './Box.module.scss'
+
+const Box = ({ children }) => {
+    return (
+        <div  className={styles.wrapper}>
+            { children }
+        </div>
+    )
+}
+
+export default Box
---
+++ next.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    experimental: { scss: true }
+}
